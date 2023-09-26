86dcc69422a83c2267b591d92eff76e992b042bd
FitoMauladi <fitomauladi45@gmail.com>
2023-09-26 15:53:54 +0700

    Tugas Biodata Pertemuan 2

---
+++ application/views/view_biodata.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Nama: Fito Mauladi Wahid</h1>
+    <h1>Kelas: 10.3A.07</h1>
+    <h1>NIM: 10220029</h1>
+</body>
+
+</html>
