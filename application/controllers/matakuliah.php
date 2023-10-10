92ea308adbe1c0012ed0fce7760c452debb227b6
FitoMauladi <fitomauladi45@gmail.com>
2023-10-10 15:25:04 +0700

    tugas pertemuan 4

--- application/controllers/matakuliah.php
+++ application/controllers/matakuliah.php
@@ -15,6 +15,20 @@ class Matakuliah extends CI_Controller
             'sks' => $this->input->post('sks'),
         ];
 
+        $this->form_validation->set_rules('kode', 'kode matakuliah', 'required|min_length[3]', [
+            'required' => 'kode mata kuliah harus diisi',
+            'min_length' => 'kode terlalu pendek'
+        ]);
+
+        $this->form_validation->set_rules('nama', 'nama matakuliah', 'required|min_length[3]', [
+            'required' => 'nama mata kuliah harus diisi',
+            'min_length' => 'nama terlalu pendek'
+        ]);
+
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+        }
+
         $this->load->view('view-data-matakuliah', $data);
     }
 }
